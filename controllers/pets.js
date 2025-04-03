a92e954fecef1aa8a1397792a0648e3b3e6597d8
erlopez11 <lopezelainy@gmail.com>
2025-04-02 22:07:23 -0500

    wip: define create pet route

--- controllers/pets.js
+++ controllers/pets.js
@@ -2,7 +2,16 @@ const express = require('express');
 const router = express.Router();
 const Pet = require('../models/pet');
 
-
+//POST /pets Create Route
+router.post('/', async (req, res) => {
+    try {
+        const createPet = await Pet.create(req.body);
+        res.status(201).json(createPet);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: error.message});
+    }
+});
 
 
 module.exports = router;
